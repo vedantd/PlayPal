ef4c791ddc705440fa1d951ab4af52b7bc2da6c5
vedantdalvi <dalvi.v@northeastern.edu>
2023-12-17 06:29:55 -0500

    SpringBoot application initialised

---
+++ src/main/java/com/vedantd/playpal/PlayPalApplication.java
@@ -0,0 +1,13 @@
+package com.vedantd.playpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayPalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayPalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vedantd/playpal/PlayPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedantd.playpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayPalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
